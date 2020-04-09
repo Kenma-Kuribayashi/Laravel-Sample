ed4289d40bfad4e1fc5575777ea286a4136092d0
K <ik3xt80qm@icloud.com>
2020-04-09 22:02:05 +0900

    Article系のサービスクラスにDOCコメント追加

--- app/Services/GetArticle.php
+++ app/Services/GetArticle.php
@@ -7,6 +7,13 @@ use App\Article;
 
 class GetArticle {
 
+  /**
+   * 特定の記事を取得する。
+   * 
+   * @var Collection
+   * @param int $article_id
+   * @return Collection
+   */
   public function get_article(int $article_id): Article {
 
     return Article::where('id', $article_id)
--- app/Services/StoreArticle.php
+++ app/Services/StoreArticle.php
@@ -3,15 +3,22 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Auth;
+use App\Article;
 
 
 class StoreArticle {
   public function store(array $params, ?array $tags) {
     //Auth::user()のような形で Auth ファサードを使うとログイン中のユーザーの情報を取得できる。articlesメソッドはArticleモデルとTagモデルが多対多の構造をつくる。
-    $article = Auth::user()->articles()->create($params);  //ArticleRequestのrulesに基づいて送られてきた値をチェックする。
+    /**
+     * ArticleRequestのrulesに基づいて送られてきた値をチェックする。
+     * 
+     * @var App\Article
+     */
+    $article = Auth::user()->articles()->create($params);
+
     $article->tags()->attach($tags); //attach多対対のとき
   }
 }
 
 
- ?>
+?>
