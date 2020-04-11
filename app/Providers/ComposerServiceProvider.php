2a34890595a62ee8450a063a78da6ac30f2fe75b
K <ik3xt80qm@icloud.com>
2020-04-11 15:29:38 +0900

    viewコンポーザー修正(article系全てのbladeにログインユーザ情報渡す)

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -25,7 +25,7 @@ class ComposerServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer(
-            ['navbar', 'articles.show'],
+            ['navbar', 'articles.*'],
             'App\Http\ViewComposers\AuthComposer'
         );
     }
