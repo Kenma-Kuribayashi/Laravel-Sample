bceea6509e071d67597a4b069820dd072056f1b1
K <ik3xt80qm@icloud.com>
2020-04-14 21:57:49 +0900

    GetBrowsingHistoriesRepositoryInterfaceのメソッドに返り値指定

--- app/Repositories/Interfaces/GetBrowsingHistoriesRepositoryInterface.php
+++ app/Repositories/Interfaces/GetBrowsingHistoriesRepositoryInterface.php
@@ -3,5 +3,10 @@ namespace App\Repositories\Interfaces;
 
 interface GetBrowsingHistoriesRepositoryInterface
 {
+    /**
+     * 
+     * @param integer $user_id
+     * @return Collection
+     */
     public function getBrowsingHistories(int $user_id);
 }
