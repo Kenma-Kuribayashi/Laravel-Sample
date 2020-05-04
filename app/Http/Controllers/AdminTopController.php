cd1c86c7c62e2c2a418e2a9cf2de16726fc54bed
K <ik3xt80qm@icloud.com>
2020-05-04 18:16:10 +0900

    管理者のミドルウェア作成

---
+++ app/Http/Controllers/AdminTopController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Services\AdminTop\GetUsers;
+
+
+class AdminTopController extends Controller
+{
+  public function __construct() {
+    $this->middleware('check-admin'); 
+  }
+
+  public function index(GetUsers $getUsers) {
+
+    $users = $getUsers->getUsers();
+
+    //dd($users);
+
+    return view('adminTop.index', ['users' => $users]);
+  }
+
+}
