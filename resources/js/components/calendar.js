f1954198eb2aeaa59b6f38fa75ab06eb016a990b
K <ik3xt80qm@icloud.com>
2020-05-18 16:10:45 +0900

    [WIP]カレンダー改修

--- resources/js/components/calendar.js
+++ resources/js/components/calendar.js
@@ -1,4 +1,5 @@
 $(() => {
+//曜日は0:日,1:月
 const WEEK_DAYS = 7;
 const date = new Date();
 //年を4桁の整数で取得
@@ -10,6 +11,9 @@ const startDate = new Date(year, month - 1, 1); // 月の最初の日を取得
 const endDate = new Date(year, month, 0); // 月の最後の日を取得
 const endDayCount = endDate.getDate(); // 月の末日
 const startDay = startDate.getDay(); // 月の最初の曜日を取得
+const endDay = endDate.getDay(); // 月の末日の曜日を取得
+const prevMonthEndDate = new Date(year, month - 1, 0);
+const prevMonthEndDayCount = prevMonthEndDate.getDate();
 
 $('#year-month').text(`${year}年${month}月`);
 $('.non-display').removeClass('non-display');
@@ -47,14 +51,26 @@ const rowRenderer = (cellListPerWeek) => {
  * @param {*} maxDate 
  * @param {*} startDay 
  */
-const makeDayNumberArray = (maxDate, startDay) => {
+const makeDayNumberArray = (maxDate, startDay, endDay, prevMonthMaxDate) => {
+
+    //今月のカレンダーに表示される先月の日数
+    var numberOfDaysLeftInLastMonth = startDay;
+    //今月のカレンダーに表示される来月の日数
+    var numberOfDaysLeftInNextMonth = 6 - endDay;
+
     return Array.from(
         {
-            length: maxDate + startDay + ((maxDate + startDay) % WEEK_DAYS)
+            //31+5+(31+5)%7=37
+            length: maxDate + numberOfDaysLeftInLastMonth + numberOfDaysLeftInNextMonth
         },
         (value, index) => {
+            //先月の日にちを入れる
+            if (index < startDay) {
+                return prevMonthMaxDate - startDay + index + 1;
+            }
+            //来月の日にちを入れる
             if (index + 1 - startDay > maxDate) {
-                return 0;
+                return index - maxDate - startDay + 1;
             }
             return index + 1 - startDay;
         }
@@ -73,8 +89,12 @@ Object.defineProperty(Array.prototype, 'chunk', {
     }
 });
 
-var calendarHtml = makeDayNumberArray(endDayCount, startDay).map(positiveNumberCellRenderer).chunk(7).map(rowRenderer);
+// console.log(endDayCount, startDay, endDay);
+// var calendarHtml = makeDayNumberArray(endDayCount, startDay, endDay, prevMonthEndDayCount);
+// console.log(calendarHtml);
 
-$("#calendar").html(contents);
+var calendarHtml = makeDayNumberArray(endDayCount, startDay, endDay, prevMonthEndDayCount).map(positiveNumberCellRenderer).chunk(7).map(rowRenderer);
+
+$("#calendar").html(calendarHtml);
 
 });
