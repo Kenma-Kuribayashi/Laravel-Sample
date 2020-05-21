b43cb7e10a6b1b09dfb3e290cc07f774cc14d314
K <ik3xt80qm@icloud.com>
2020-05-21 16:30:37 +0900

    投稿者登録するform修正

--- resources/views/AdminTop/index.blade.php
+++ resources/views/AdminTop/index.blade.php
@@ -11,8 +11,9 @@
 @if($user->isContributor())
 <div>○</div>
 @else
-<form method="PUT" action="/users/{{ $user->getId() }}">
+<form method="post" action="/users/{{ $user->getId() }}">
   {{ csrf_field() }}
+  <input type="hidden" name="_method" value="PUT">
   <input type="submit" value="投稿者登録する" />
 </form>
 
