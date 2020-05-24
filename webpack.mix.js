0368f1ea668809a903826f5d6f07a5ca014de36e
K <ik3xt80qm@icloud.com>
2020-05-24 23:04:38 +0900

    コンパイル後はcalendar.jsをarticleList.jsに記載するよう変更

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/components/calendar.js', 'public/js/components/')
+    .js('resources/js/page/articleList.js', 'public/js/page/')
     .sass('resources/sass/app.scss', 'public/css');
