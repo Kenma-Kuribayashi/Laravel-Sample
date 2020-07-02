eb98d512bbdaa8b797dbf68a997fa8d3366c68a0
K <ik3xt80qm@icloud.com>
2020-07-02 21:26:26 +0900

    カレンダーの先月翌月ボタン作成,今日の日付にcss当てる

--- resources/js/components/Calendar.vue
+++ resources/js/components/Calendar.vue
@@ -3,14 +3,15 @@
     <div>
       <h3 clsss="calendar-title">{{ year }}年{{ month }}月</h3>
     </div>
-    <button type="button" class="btn btn-primary">&larr;</button>
-    <button @click="onClickButton" type="button" class="btn btn-primary">&rarr;</button>
+    <button @click="onClickPrevButton" type="button" class="btn btn-primary">&larr;</button>
+    <button @click="onClickNextButton" type="button" class="btn btn-primary">&rarr;</button>
     <table>
       <tr>
         <td class="calendar-td" v-for="day in weeks" :key="day">{{ day }}</td>
       </tr>
-      <tr v-for="week in calendarMake" :key="week">
-        <td v-for="day in week" :key="day" class="calendar-td">{{ day }}</td>
+      <tr v-for="(week, index) in calendarMake" :key="index">
+        <!-- <td v-for="day in week" :key="day" class="calendar-td">{{ day }}</td> -->
+        <td v-for="dayObject in week" :key="dayObject.date" :class="`calendar-td ${dayObject.month !== month ? 'gray' : ''} ${dayObject.year === todayDate.year && dayObject.month === todayDate.month && dayObject.date === todayDate.date ? 'red' : ''}`">{{ dayObject.date }}</td>
       </tr>
     </table>
   </div>
@@ -22,21 +23,6 @@ export default {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
-    const todayDate = { year, month, date: date.getDate() };
-    // 表示する月の最初の日を取得
-    const startDate = new Date(year, month - 1, 1);
-    // 表示する月の最後の日を取得
-    const endDate = new Date(year, month, 0);
-    // 表示する月の最後の日の日にちを取得
-    const endDayCount = endDate.getDate();
-    // 表示する月の最初の曜日を取得
-    const startDay = startDate.getDay();
-    // 表示する月の末日の曜日を取得
-    const endDay = endDate.getDay();
-    // 表示する先月の最後の日を取得
-    const prevMonthEndDate = new Date(year, month - 1, 0);
-    // 表示する先月の最後の日の日にちを取得
-    const prevMonthEndDayCount = prevMonthEndDate.getDate();
 
     return {
       weeks: ["日", "月", "火", "水", "木", "金", "土"],
@@ -44,19 +30,56 @@ export default {
       date,
       year,
       month,
-      todayDate,
-      startDate,
-      endDate,
-      endDayCount,
-      startDay,
-      endDay,
-      prevMonthEndDate,
-      prevMonthEndDayCount
+      todayDate: null
+      // startDate: date,
+      // endDate: date,
+      // endDayCount: 0,
+      // startDay: 0,
+      // endDay: 0,
+      // prevMonthEndDate: 30,
+      // prevMonthEndDayCount: 30
     };
   },
+  // initialize
   methods: {
-    onClickButton() {
-      this.day = 1000;
+    initialize() {
+      this.todayDate = {
+        year: this.year,
+        month: this.month,
+        date: this.date.getDate()
+      };
+      // 表示する月の最初の日を取得
+      this.startDate = new Date(this.year, this.month - 1, 1);
+      // 表示する月の最後の日を取得
+      this.endDate = new Date(this.year, this.month, 0);
+      // 表示する月の最後の日の日にちを取得
+      this.endDayCount = this.endDate.getDate();
+      // 表示する月の最初の曜日を取得
+      this.startDay = this.startDate.getDay();
+      // 表示する月の末日の曜日を取得
+      this.endDay = this.endDate.getDay();
+      // 表示する先月の最後の日を取得
+      this.prevMonthEndDate = new Date(this.year, this.month - 1, 0);
+      // 表示する先月の最後の日の日にちを取得
+      this.prevMonthEndDayCount = this.prevMonthEndDate.getDate();
+    },
+    onClickNextButton() {
+      this.month += 1;
+      if (this.month === 13) {
+        this.year += 1;
+        this.month = 1;
+      }
+      this.days = [];
+      this.initialize();
+    },
+    onClickPrevButton() {
+      this.month -= 1;
+      if (this.month === 0) {
+        this.year -= 1;
+        this.month = 12;
+      }
+      this.days = [];
+      this.initialize();
     },
     makeDayNumberArray(maxDate, startDay, endDay, prevMonthMaxDate) {
       //今月のカレンダーに表示される先月の日数
@@ -95,22 +118,33 @@ export default {
       );
     },
     dateRenderer(payload) {
-      return this.days.push(payload.date);
+        console.log(payload);
+      return this.days.push(payload);
     }
   },
   computed: {
     calendarMake() {
+      // initializeの実行前にはカレンダーを作らない
+      if (this.todayDate === null) {
+        return [];
+      }
       this.makeDayNumberArray(
         this.endDayCount,
         this.startDay,
         this.endDay,
         this.prevMonthEndDayCount
       ).map(this.dateRenderer);
-      
+
       return this.days.chunk(7);
+    },
+    // 他のthis.XXXXによって計算される変数はすべてcomputedに入れて良い
+    _startDate() {
+      return new Date(this.year, this.month - 1, 1)
     }
   },
-  mounted() {}
+  mounted() {
+    this.initialize();
+  }
 };
 
 /**
@@ -125,17 +159,6 @@ Object.defineProperty(Array.prototype, "chunk", {
   }
 });
 
-
-//         this.bindEvent();
-//     }
-
-//     render() {
-//         $('#year-month').text(`${this.year}年${this.month}月`);
-//         $('.non-display').removeClass('non-display');
-//         var calendarHtml = this.privateMakeDayNumberArray(endDayCount, startDay, endDay, prevMonthEndDayCount).map(this.privateDateRenderer.bind(this)).chunk(7).map(this.privateRowRenderer);
-
-//         $("#calendar").html(calendarHtml);
-//     }
 
 //     /**
 //      * このメソッドが知っていること
@@ -177,26 +200,12 @@ Object.defineProperty(Array.prototype, "chunk", {
 //   return true;
 // };
 
-//     bindEvent() {
-//         //先月ボタンが押されたら表示されている前月のカレンダーを作成する
-//         $('#prev-month').click(function () {
-//             this.month -= 1;
-//             if (this.month === 0) {
-//                 this.year -= 1;
-//                 this.month = 12;
-//             }
-//             this.render();
-//         }.bind(this));
-
-//         //来月ボタンが押されたら表示されている来月のカレンダーを作成する
-//         $('#next-month').click(function () {
-//             this.month += 1;
-//             if (this.month === 13) {
-//                 this.year += 1;
-//                 this.month = 1;
-//             }
-//             this.render();
-//         }.bind(this));
-//     }
-// };
 </script>
+<style scoped>
+.gray {
+    color: #808080;
+}
+.red {
+    color: red;
+}
+</style>
