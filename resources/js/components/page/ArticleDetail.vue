e76224a1caccf449d7e62398ad4e22fcd6af9f6b
K <ik3xt80qm@icloud.com>
2020-08-17 19:46:38 +0900

    記事詳細画面

--- resources/js/components/page/ArticleDetail.vue
+++ resources/js/components/page/ArticleDetail.vue
@@ -1,3 +1,38 @@
+<template>
+  <div>
+    <tag-list-tab v-bind:currentTag="currentTag" @click-tab="onClickTabButton" />
+
+    <!-- <ul v-show="hasErrors">
+      <li v-for="(error, index) in errors" :key="index">{{ error }}
+      </li>
+    </ul>-->
+
+    <div v-show="hasMessage">
+      <div class="alert alert-primary">{{ message }}</div>
+    </div>
+
+    <div class="panel panel-default">
+      <figure v-show="article.hasImagePath">
+        <img :src="article.imagePath" width="533px" height="400px" />
+      </figure>
+    </div>
+
+    <h1>{{ article.title }}</h1>
+
+    <article>
+      <div class="body">{{ article.body }}</div>
+    </article>
+    <div v-show="article.hasTags">
+      <h5>Tags:</h5>
+      <ul>
+        <li v-for="(tag) in article.tags" :key="tag.id">{{ tag.name }}</li>
+      </ul>
+    </div>
+
+    <div v-if="auth.length !== 0">
+      <!-- 投稿したユーザーのidとログインユーザーのidが一致する場合表示 -->
+
+      <a v-show="isAuthor" href="#" class="btn btn-primary">編集</a>
       <!--管理者の場合は投稿者でなくても表示-->
       <button
         v-show="isAuthor || auth.is_admin"
@@ -5,6 +40,36 @@
         @click="onClickDeleteButton()"
       >削除</button>
 
+      <!-- <form id="delete-form" :action="`api/articles/${articleId}`" method="post">
+        <input type="hidden" name="_token" :value="csrf" /> -->
+        <!-- <input type="hidden" name="_method" value="DELETE"> -->
+        <!-- <input type="submit" value="delete!!" />
+      </form> -->
+
+      <br />
+      <br />
+      <div v-show="isAuthor">
+        <div class="red">※画像ファイルは50KB以下でお願いします。(現在改良中のため)</div>
+        <!-- <form
+        action="/upload/{{ $article->getArticleId() }}"
+        method="POST"
+        enctype="multipart/form-data"
+        class="post_form"
+        >-->
+        <div class="form_parts">
+          <label for="photo">画像ファイル:</label>
+          <input type="file" class="form-control" name="image" />
+          <br />
+          <!-- {{ csrf_field() }} -->
+          <button class="btn btn-success">投稿</button>
+        </div>
+        <!-- </form> -->
+      </div>
+    </div>
+
+    <router-link :to="{ name: 'articleList'}" class="btn btn-secondary float-right">一覧へ戻る</router-link>
+
+    <br />
     <div class>おすすめの記事</div>
 
     <div class="articles" v-for="(article, index) in recommendedArticles" :key="index">
@@ -25,11 +90,65 @@
         <div class="created-time">{{ createdAt(article.created_at) }}</div>
       </article>
     </div>
+  </div>
+</template>
+
+<script>
+import TagListTab from "../parts/TagListTab";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+dayjs.locale("ja");
+
+export default {
+  components: {
+    TagListTab,
+  },
+  props: {
+    auth: {
+      type: Object | Array,
+    },
+    errors: {
+      type: Object | Array,
+    },
+  },
+  data() {
+    return {
+      currentTag: "主要",
+      article: "",
+      articleId: "",
+      // errors: [],
       recommendedArticles: [],
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content"),
+      message: ""
+
+    };
+  },
+  mounted() {
+    this.articleId = this.$route.params.articleId;
+
+    //console.log(this.auth);
+
+    axios.get("/api/get/article/" + this.articleId).then(({ data }) => {
+      this.article = data.data;
+    });
+
     //おすすめ記事の取得
     axios.get("/api/recommend_article/" + this.articleId).then(({ data }) => {
       this.recommendedArticles = data;
     });
+
+  },
+  methods: {
+    createdAt(date) {
+      return dayjs(date).format("M/D(ddd) HH:mm");
+    },
+    onClickTabButton(tag) {
+      this.currentTag = tag;
+
+      this.$router.push(`/?tag=${tag}`);
+    },
     onClickRecommendArticleButton(articleId) {
       this.articleId = articleId;
       this.$router.push(`/articles/${articleId}`);
@@ -46,8 +165,33 @@
     onClickDeleteButton() {
        axios.delete("/api/articles/" +this.articleId).then(({data})  => {
         if (data === "success") {
-          this.message = "記事を削除しました。"
           this.$router.push({ path: '/', query: { message: '記事を削除しました。' } });
         }
       });
     },
+  },
+  computed: {
+    // hasErrors() {
+    //   if (this.errors[0]) {
+    //     return true;
+    //   }
+    //   return false;
+    // },
+    hasMessage() {
+      if (this.message != "") {
+        return true;
+      }
+      return false;
+    },
+    isAuthor() {
+      if (this.article.userId === this.auth.id) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
