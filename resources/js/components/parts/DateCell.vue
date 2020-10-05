b78c4babc0d07e78730bb48c628ca1e6a3861be6
K <ik3xt80qm@icloud.com>
2020-10-05 16:28:31 +0900

    カレンダーのvueのディレクトリ変更

---
+++ resources/js/components/parts/DateCell.vue
@@ -0,0 +1,32 @@
+<template>
+  <td :class="`calendar-td ${day.month !== currentMonth ? 'gray' : ''} ${todayClassName}`">{{ day.date }}</td>
+</template>
+
+<script>
+export default {
+  props:{
+    day: Object,
+    todayDate: Object,
+    currentMonth: Number,
+  },
+  methods: {
+  },
+  computed: {
+    todayClassName() {
+      if (
+        this.day.year === this.todayDate.year &&
+        this.day.month === this.todayDate.month &&
+        this.day.date === this.todayDate.date
+      ) {
+        return "red";
+      } else {
+        return "";
+      }
+    }
+  },
+  mounted() {}
+};
+
+</script>
+<style scoped>
+</style>
