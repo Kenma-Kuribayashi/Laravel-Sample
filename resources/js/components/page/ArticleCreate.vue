2f8aa71fa21bee42fd38bca99742f8f0738fd4f2
K <ik3xt80qm@icloud.com>
2021-01-11 11:59:21 +0900

    リファクタ前

--- resources/js/components/page/ArticleCreate.vue
+++ resources/js/components/page/ArticleCreate.vue
@@ -1,142 +1,196 @@
 <template>
-    <div>
+  <div>
+    <h1>記事の新規投稿</h1>
 
-        <h1>記事の新規投稿</h1>
-
-        <div v-show="hasMessage">
-            <div class="alert alert-primary">{{ message }}</div>
-        </div>
-
-        <p>タイトル:</p>
-        <input class="form-control" name="title" v-model="title"/>
-
-        <p>本文:</p>
-        <textarea class="form-control" name="body" v-model="body"></textarea>
-
-        <p>記事の公開日:</p>
-        <input class="form-control" type="date" name="published_at" v-model="publishedAt">
-
-        <p>タグ:</p>
-        <select class="form-control" v-model="tagId">
-            <option disabled value="">1つ選択してください</option>
-            <option v-for="tag in tags" v-bind:value="tag.id">{{ tag.name }}</option>
-        </select>
-
-        <div class="form_parts">
-            <input type="file" ref="file" @change="setImage"/>
-        </div>
-
-        <div class="btn-container">
-            <button @click="onClickSubmitButton()" class="btn btn-primary">新規投稿</button>
-        </div>
-
+    <div v-show="message !== ''">
+      <div class="alert alert-primary">{{ message }}</div>
     </div>
+
+    <div v-for="(error, index) in errors" :key="index" class="text-danger">
+      {{ error }}
+    </div>
+
+    <modal v-bind:message="message" v-show="showModal" />
+
+    <ValidationProvider rules="required|min:3|max:50" v-slot="{ errors }">
+      <p>タイトル:</p>
+      <input class="form-control" name="title" v-model="title" />
+      <div class="text-danger" v-show="errors[0]">{{ errors[0] }}</div>
+    </ValidationProvider>
+
+    <ValidationProvider rules="required|max:100" v-slot="{ errors }">
+      <p>本文:</p>
+      <textarea class="form-control" name="body" v-model="body"></textarea>
+      <div class="text-danger" v-show="errors[0]">{{ errors[0] }}</div>
+    </ValidationProvider>
+
+    <ValidationProvider rules="required" v-slot="{ errors }">
+      <p>記事の公開日:</p>
+      <input
+        class="form-control"
+        type="date"
+        name="published_at"
+        v-model="publishedAt"
+      />
+      <div class="text-danger" v-show="errors[0]">{{ errors[0] }}</div>
+    </ValidationProvider>
+
+    <p>タグ:</p>
+    <select class="form-control" v-model="tagId">
+      <option disabled value="">1つ選択してください</option>
+      <option v-for="(tag, index) in tags" :key="index" :value="tag.id">
+        {{ tag.name }}
+      </option>
+    </select>
+
+    <ValidationProvider rules="required|image" v-slot="{ errors }">
+      <p>画像:</p>
+      <div class="form_parts">
+        <input type="file" name="image" ref="file" @change="setImage" />
+        <div class="text-danger" v-show="errors[0]">{{ errors[0] }}</div>
+      </div>
+    </ValidationProvider>
+
+    <div class="btn-container">
+      <button
+        @click="onClickSubmitButton()"
+        :disabled="!ablePost"
+        class="btn btn-primary"
+      >
+        新規投稿
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Modal from "../parts/Modal";
+import axios from "axios";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
-
-dayjs.locale("ja");
+import { ValidationProvider, extend } from "vee-validate";
+import { required, min, max } from "vee-validate/dist/rules";
+extend("required", {
+  ...required,
+  message: "{_field_}は必須です",
+});
+extend("max", {
+  ...max,
+  message: "{_field_}は{length}文字以内で入力してください",
+});
+extend("min", {
+  ...min,
+  message: "{_field_}は{length}文字以上で入力してください",
+});
 
 export default {
-    components: {},
-    props: {
-        auth: {
-            type: Object,
-            required: true,
-        }
+  components: {
+    ValidationProvider,
+    Modal,
+  },
+  props: {
+    auth: {
+      type: Object,
+      required: true,
     },
-    data() {
-        return {
-            // errors: [],
-            csrf: document
-                .querySelector('meta[name="csrf-token"]')
-                .getAttribute("content"),
-            message: "",
-            title: "",
-            body: "",
-            publishedAt: "",
-            tags: [],
-            image_data: {
-                image: "",
-                name: "",
-            },
-            tagId: 1,
-        };
-    },
-    mounted() {
-        this.publishedAt = dayjs().format('YYYY-MM-DD');
+    // errors: {
+    //     type: Array | Object,
+    //     required: true,
+    // }
+  },
+  data() {
+    return {
+      errors: [],
+      csrf: document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content"),
+      message: "",
+      title: "",
+      body: "",
+      publishedAt: "",
+      tags: [],
+      image_data: {
+        image: "",
+        name: "",
+      },
+      tagId: 1,
+      showModal: false,
+    };
+  },
+  mounted() {
+    this.publishedAt = dayjs().format("YYYY-MM-DD");
 
-        this.$http.get("/api/tags").then((response) => {
-            this.tags = response.data.data;
+    this.$http.get("/api/tags").then((response) => {
+      this.tags = response.data.data;
+    });
+  },
+  methods: {
+    async onClickSubmitButton() {
+      //console.log(this.image_data);
+
+      //const FormData = require('form-data');
+      const form = new FormData();
+      form.append("title", this.title);
+      form.append("body", this.body);
+      form.append("published_at", this.publishedAt);
+      form.append("tagId", this.tagId);
+      form.append("image", this.image_data);
+
+      try {
+        this.isSending = true;
+
+        await axios.post("/api/articles/", form, {
+          headers: { "content-type": "multipart/form-data;" },
         });
-    },
-    methods: {
-        onClickSubmitButton() {
-            console.log(this.image_data);
 
-            //const FormData = require('form-data');
-            const form = new FormData();
-            form.append('title', this.title);
-            form.append('body', this.body);
-            form.append('published_at', this.publishedAt);
-            form.append('tagId', this.tagId);
-            form.append('image', this.image_data);
+        this.message = "記事の新規作成しました。";
+        this.showModal = true;
+      } catch (err) {
+       // console.log(this.errors, err.response.data.errors);
 
-            axios.post("/api/articles/",
-                form,
-                { headers:  {'content-type': 'multipart/form-data;'}}
-            )
-                .then(() => {
-                    this.isModal = true;
-                }).catch(err => {
-                this.message = "記事の新規作成に失敗しました。"
-            }).finally(() => this.isSending = false);
-        },
-        setImage(e) {
-            const files = this.$refs.file;
-            this.image_data = files.files[0];
-            //const fileImg = files.files[0];
-            //console.log(fileImg);
-            // if (fileImg.type.startsWith("image/")) {
-            //     this.image_data.image = window.URL.createObjectURL(fileImg);
-            //     this.image_data.name = fileImg.name;
-            //     this.image_data.type = fileImg.type;
-            // }
+        for (let k in err.response.data.errors) {
+          //console.log(err.response.data.errors[k]);
+          err.response.data.errors[k].forEach((a) => {
+            this.errors.push(a);
+          });
+          this.message = "記事の新規作成に失敗しました。";
         }
+      }
+      this.isSending = false;
     },
-    computed: {
-        hasMessage() {
-            if (this.message !== "") {
-                return true;
-            }
-            return false;
-        }
-        ,
-    }
-    ,
-    watch: {}
-    ,
-}
-;
+    //setImage(e) {
+    setImage() {
+      //console.log(this.$refs.file);
+      const files = this.$refs.file;
+      this.image_data = files.files[0];
+      //console.log(this.image_data);
+      //const fileImg = files.files[0];
+      //console.log(fileImg);
+      // if (fileImg.type.startsWith("image/")) {
+      //     this.image_data.image = window.URL.createObjectURL(fileImg);
+      //     this.image_data.name = fileImg.name;
+      //     this.image_data.type = fileImg.type;
+      // }
+    },
+  },
+  computed: {
+    ablePost() {
+      if (
+        this.title.length >= 3 &&
+        this.title.length <= 50 &&
+        this.body !== "" &&
+        this.body.length <= 100 &&
+        this.image_data.image !== ""
+      ) {
+        return true;
+      }
+      return false;
+    },
+  },
+  watch: {},
+};
 </script>
 
 <style scoped>
-.my-modal {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.5);
-}
 
-.my-modal-dialog {
-    position: absolute;
-    top: 30px;
-    left: calc(50% - 150px);
-    width: 400px;
-    background: white;
-}
 </style>
